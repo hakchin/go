693089b54930b0e9bbcf3af6c226d205d60408e4
hakchin <hakchin@gmail.com>
2016-10-09 23:28:57 +0900

    new file:   #main.go# 	modified:   main.go

--- src/004.go.array/main.go
+++ src/004.go.array/main.go
@@ -1,17 +1,19 @@
 package main
+
 import "fmt"
+
 func main() {
-    x := [5]float64{
-        94,
-        93,
-        77,
-        82,
-        83,
-    }
-    var total float64 = 0
-    for _, value := range x {
-        total += value
-        fmt.Println(total)
-    }
-    fmt.Println(total / float64(len(x)))
+	x := [5]float64{
+		94,
+		93,
+		77,
+		82,
+		83,
+	}
+	var total float64 = 0
+	for _, value := range x {
+		total += value
+		fmt.Println(total)
+	}
+	fmt.Println(total / float64(len(x)))
 }
