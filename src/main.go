c3bdda2de373bb8eaaee2620bcb29c0162d5aba4
hakchin <hakchin@gmail.com>
2016-10-09 13:56:39 +0900

    new file:   .main.go.swp 	modified:   main.go

--- src/main.go
+++ src/main.go
@@ -2,17 +2,9 @@ package main
 import "fmt"
 
 func main() {
-    x := [5]float64{
-        1,
-        1,
-        1,
-        1,
-        1,
-    }
-    var total float64 = 0
-    for _, value := range x {
-        total += value
-        fmt.Println(total)
-    }
-    fmt.Println(total / float64(len(x)))
+    fmt.Print("Enetr a number: ")
+    var input float64
+    fmt.Scanf("%f", &input)
+    output := input * 2
+    fmt.Println(output)
 }
