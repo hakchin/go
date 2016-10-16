21d0850ed6b0ab8bed74fe54dcc5dd555e1ab677
hakchin <hakchin@gmail.com>
2016-10-16 18:20:23 +0900

    modified:   main.go

--- src/main.go
+++ src/main.go
@@ -1,10 +1,11 @@
 package main
+
 import "fmt"
 
 func main() {
-    fmt.Print("Enetr a number: ")
-    var input float64
-    fmt.Scanf("%f", &input)
-    output := input * 2
-    fmt.Println(output)
+	fmt.Print("Enetr a number: ")
+	var input float64
+	fmt.Scanf("%f", &input)
+	output := input * 2
+	fmt.Println(output)
 }
