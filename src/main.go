1a6417e6680d68d7977c558f7b06f2f5d0b1831b
hakchin <hakchin@gmail.com>
2016-11-06 23:32:54 +0900

    modified:   main 	modified:   main.go
     Changes not staged for commit:
    	modified:   appengine-guestbook-go (modified content)

--- src/main.go
+++ src/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, 세계")
+	xs := []float64{98, 93, 77, 82, 83}
+
+	total := 0.0
+	for _, v := range xs {
+		total += v
+	}
+	fmt.Println(total / float64(len(xs)))
 }
