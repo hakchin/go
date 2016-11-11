e6d4b3a8015fd63a8f282637ae43802e225c6eba
hakchin <hakchin@gmail.com>
2016-11-12 01:02:26 +0900

    modified:   main.go

--- src/main.go
+++ src/main.go
@@ -1,10 +1,16 @@
 package main
-
 import "fmt"
-
-func main() {
-	add := func(x, y int) int {
-		return x + y
+func makeEvenGenerator() func () uint{
+	i := uint(0)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
 	}
-	fmt.Println(add(21, 1))
+}
+func main() {
+	nextEven := makeEvenGenerator()
+	fmt.Println(nextEven()) //0
+	fmt.Println(nextEven()) //2
+	fmt.Println(nextEven()) //4
 }
