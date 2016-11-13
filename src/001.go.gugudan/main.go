b9b1db5f374943f7e28e9236ed46d78a7f28c212
hakchin <hakchin@gmail.com>
2016-11-13 14:05:45 +0900

    modified:   main.go

--- src/001.go.gugudan/main.go
+++ src/001.go.gugudan/main.go
@@ -1,19 +1,20 @@
 package main
+
 import "fmt"
-func dan(a int){
-    var i int
-    i = 1
-    fmt.Println(a)
-    for i < 10 {
-        fmt.Println(a, " * ", i, " = ", a * i )
-        i = i + 1
-    }
+
+func dan(a int) {
+	var i int
+	i = 1
+	fmt.Println(a)
+	for i < 10 {
+		fmt.Println(a, " * ", i, " = ", a*i)
+		i = i + 1
+	}
 }
 
-func main(){
-    var j int
-    for j = 2; j < 10; j++ {
-        dan(j)
-    }
+func main() {
+	var j int
+	for j = 2; j < 10; j++ {
+		dan(j)
+	}
 }
-
