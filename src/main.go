b7c1718cb64de141214b453029b3e159b65966d6
hakchin <hakchin@gmail.com>
2016-11-13 15:04:44 +0900

    modified:   main.go

--- src/main.go
+++ src/main.go
@@ -1,21 +1,14 @@
-// Go supports
-// <a href="http://en.wikipedia.org/wiki/Recursion_(computer_science)"><em>recursive functions</em></a>.
-// Here's a classic factorial example.
-
 package main
 
 import "fmt"
 
-// This `fact` function calls itself until it reaches the
-// base case of `fact(0)`.
-func fact(n int) int {
-	if n == 0 {
-		return 1
-	}
-	fmt.Println(n)
-	return n * fact(n-1)
+func first() {
+	fmt.Println("1st")
 }
-
+func second() {
+	fmt.Println("2nd")
+}
 func main() {
-	fmt.Println(fact(7))
+	defer second()
+	first()
 }
