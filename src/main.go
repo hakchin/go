a7ab0cafacefc8564a9bd39197bc33997dce4f9d
hakchin <hakchin@gmail.com>
2016-11-15 01:54:00 +0900

    deleted:    main 	modified:   main.go

--- src/main.go
+++ src/main.go
@@ -2,26 +2,13 @@ package main
 
 import "fmt"
 
-func zeroval(ival int) {
-	ival = 0
+func one(xPtr *int) {
+	*xPtr = 1
 }
-
-func zeroptr(iptr *int) {
-	*iptr = 0
-}
-
 func main() {
-	i := 1
-	fmt.Println("initial:", i)
-
-	zeroval(i)
-	fmt.Println("zeroval:", i)
-
-	// The `&i` syntax gives the memory address of `i`,
-	// i.e. a pointer to `i`.
-	zeroptr(&i)
-	fmt.Println("zeroptr:", i)
-
-	// Pointers can be printed too.
-	fmt.Println("pointer:", &i)
+	xPtr := new(int)
+	fmt.Println(xPtr)
+	fmt.Println(*xPtr)
+	one(xPtr)
+	fmt.Println(*xPtr) // x is 1
 }
