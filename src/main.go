60f64b84d771978bcfc7abc293a5e83f01a4f20b
hakchin <hakchin@gmail.com>
2016-11-26 00:34:23 +0900

    modified:   main.go 	modified:   main.go.old

--- src/main.go
+++ src/main.go
@@ -1,14 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func f(n int) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(n, ":", i)
-	}
-}
 func main() {
-	go f(4)
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "from 1"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+	go func() {
+		for {
+			c2 <- "from 2"
+			time.Sleep(time.Second * 3)
+		}
+	}()
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println(msg1)
+			case msg2 := <-c2:
+				fmt.Println(msg2)
+			}
+		}
+	}()
+
 	var input string
 	fmt.Scanln(&input)
 }
