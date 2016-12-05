6a427cf0888d947ca2118886d31d03ea96cc58b2
hakchin <hakchin@gmail.com>
2016-12-05 09:13:19 +0900

    modified:   main.go

--- src/main.go
+++ src/main.go
@@ -1,32 +1,61 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
-	"sort"
+	"net"
 )
 
-type Person struct {
-	Name string
-	Age  int
-}
-
-type ByName []Person
-
-func (this ByName) Len() int {
-	return len(this)
-}
-func (this ByName) Less(i, j int) bool {
-	return this[i].Name < this[j].Name
-}
-func (this ByName) Swap(i, j int) {
-	this[i], this[j] = this[j], this[i]
-}
-
-func main() {
-	kids := []Person{
-		{"Jill", 9},
-		{"Jack", 10},
+func server() {
+	// listen on a port
+	ln, err := net.Listen("tcp", ":9999")
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	sort.Sort(ByName(kids))
-	fmt.Println(kids)
+	for {
+		// accept a connection
+		c, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// handle the connection
+		go handleServerConnection(c)
+	}
+}
+func handleServerConnection(c net.Conn) {
+	// receive the message
+	var msg string
+	err := gob.NewDecoder(c).Decode(&msg)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Received", msg)
+	}
+	c.Close()
+}
+func client() {
+	// connect to the server
+	c, err := net.Dial("tcp", "127.0.0.1:9999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// send the message
+	msg := "Hello World"
+	fmt.Println("Sending", msg)
+	err = gob.NewEncoder(c).Encode(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+	c.Close()
+}
+func main() {
+	go server()
+	go client()
+
+	var input string
+	fmt.Scanln(&input)
 }
