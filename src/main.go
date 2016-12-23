e93475ceaee589a3a770c3b702f2acce6e72dde2
hakchin <hakchin@gmail.com>
2016-12-23 09:28:57 +0900

    modified:   main.go

--- src/main.go
+++ src/main.go
@@ -1,7 +1,16 @@
+// Echo1 prints its command-line arguments.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, 세계")
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
