fd7752650a548e1028b4518b49b31136ff568122
hakchin <hakchin@gmail.com>
2017-01-08 23:55:28 +0900

    deleted:    ../.main.go.swp 	modified:   ../main.go

--- src/main.go
+++ src/main.go
@@ -1,11 +1,29 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 33.
+//!+
+
+// Echo4 prints its command-line arguments.
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", "------", "separator")
 
 func main() {
-	x := 1
-	p := &x
-	fmt.Println(*p)
-	*p = 2
-	fmt.Println(x)
+	fmt.Println(*n)
+	fmt.Println(*sep)
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
 }
+
+//!-
