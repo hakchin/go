e6e108d6ac07dcd32636b5535b543a7100dc1993
hakchin <hakchin@gmail.com>
2017-01-15 17:35:21 +0900

    modified:   .DS_Store 	new file:   src/com/ppoppo/tempconv/conv.go 	new file:   src/com/ppoppo/tempconv/tempconv.go 	modified:   src/main.go

--- src/main.go
+++ src/main.go
@@ -1,33 +1,11 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 33.
-//!+
-// usage : go run main.go a bc def
-// usage : go run main.go -n a bc def
-// usage : go run main.go -s / a bc def
-// usage : go run main.go -help
-
-// Echo4 prints its command-line arguments.
 package main
 
 import (
-	"flag"
+	"com/ppoppo/stringutil"
 	"fmt"
-	"strings"
 )
 
-var n = flag.Bool("n", false, "omit trailing newline")
-var sep = flag.String("s", "------", "separator")
-
 func main() {
-	fmt.Println(*n)
-	fmt.Println(*sep)
-	flag.Parse()
-	fmt.Print(strings.Join(flag.Args(), *sep))
-	if !*n {
-		fmt.Println()
-	}
+	//	fmt.Printf(stringutil.Reverse("!.oG ,olleH"))
+	fmt.Println(stringutil.Reverse("!.oG ,olleH"))
 }
-
-//!-
