aaf1824c0d06b6cbf65b15e521bbb9d700315940
hakchin <hakchin@gmail.com>
2017-01-17 21:14:40 +0900

    modified:   main.go 	deleted:    ../.DS_Store

--- src/main.go
+++ src/main.go
@@ -1,11 +1,29 @@
+// usage : go run main.go 36.5
+// Cf converts its numeric argument to Celsius and Fahrenheit.
 package main
 
 import (
-	"com/ppoppo/stringutil"
+	"com/ppoppo/tempconv"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	//	fmt.Printf(stringutil.Reverse("!.oG ,olleH"))
-	fmt.Println(stringutil.Reverse("!.oG ,olleH"))
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+
+	i, err := strconv.Atoi("-42")
+	s := strconv.Itoa(-42)
+	fmt.Println(i)
+	fmt.Println(s)
+	fmt.Println(err)
 }
