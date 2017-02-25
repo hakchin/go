77b0becfc33b70f01bafd2e44432b9604e33e134
hakchin <hakchin@gmail.com>
2017-02-25 23:59:27 +0900

    modified:   ../../.DS_Store 	deleted:    .main.go.swp 	modified:   main.go

--- src/999.go.test/main.go
+++ src/999.go.test/main.go
@@ -1,20 +1,21 @@
-//
+// 슬라이스 삽입
 package main
 
 import "fmt"
 
 func main() {
-	var a [5]int                 // 길이가 5인 int 형 배열 선언
-	b := [3]int{1, 2, 3}         // 배열 선언과 동시에 값을 초기화
-	c := [3]int{1, 2}            // 초깃값을 지정하지 않으면 0으로 초기화됨
-	d := [...]int{4, 5, 6, 7, 8} // ...를 사용하여 배열의 길이를 지정
-	e := [3][3]int{              // 다차원 배열 선언
-		{1, 2, 3},
-		{3, 4, 5}, // 요소를 여러 줄로 표기할 때, 요소의 마지막에 콤마를 붙여야 함
-	}
-	fmt.Printf("%-10T %d %v\n", a, len(a), a)
-	fmt.Printf("%-10T %d %v\n", b, len(b), b)
-	fmt.Printf("%-10T %d %v\n", c, len(c), c)
-	fmt.Printf("%-10T %d %v\n", d, len(d), d)
-	fmt.Printf("%-10T %d %v\n", e, len(e), e)
+	s := []int{1, 2, 3, 4, 5}
+	s = insert(s, []int{-3, -2}, 0) // s: [-3 -2 1 2 3 4 5]
+	fmt.Println(s)
+
+	s = insert(s, []int{0}, 2) // s: [-3 -2 0 1 2 3 4 5]
+
+	fmt.Println(s)
+
+	s = insert(s, []int{6, 7}, len(s)) // s: [-3 -2 1 2 3 4 5 6 7]
+
+	fmt.Println(s)
+}
+func insert(s, new []int, index int) []int {
+	return append(s[:index], append(new, s[index:]...)...)
 }
