fc6b8d9b3e8216708142501f1b67c15fe780e180
hakchin <hakchin@gmail.com>
2017-03-16 00:06:01 +0900

    modified:   ../com-appspot-bridgebank/app.yaml 	modified:   ../com-appspot-hakchin/hello.go

--- src/com-appspot-hakchin/hello.go
+++ src/com-appspot-hakchin/hello.go
@@ -1,14 +1,24 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"html/template"
+	"net/http"
 )
 
-func init() {
-    http.HandleFunc("/", handler)
-}
+//func init() {
+//	http.HandleFunc("/", handler)
+//}
+//
+//func handler(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprint(w, "Hello, world!")
+//}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
 }
