ae1810af8929e2ffea5c8aa15995738bd3955b85
hakchin <hakchin@gmail.com>
2017-03-24 13:16:32 +0900

    modified:   999.go.test/.main.go.swp 	modified:   999.go.test/main.go

--- src/999.go.test/main.go
+++ src/999.go.test/main.go
@@ -4,8 +4,23 @@ import "fmt"
 
 func main() {
 	numberMap := map[string]int{}
+	numberMap["zero"] = 0
 	numberMap["one"] = 1
 	numberMap["two"] = 2
-	numberMap["three"] = 3
-	fmt.Println(numberMap)
+	fmt.Println(numberMap["zero"])
+	fmt.Println(numberMap["one"])
+	fmt.Println(numberMap["two"])
+
+	if v, ok := numberMap["three"]; ok {
+		fmt.Println("three' is in numberMap.value:", v)
+	} else {
+		fmt.Println("three' is not in numberMap")
+		fmt.Println(ok)
+	}
+	if v, ok := numberMap["two"]; ok {
+		fmt.Println("two' is in numberMap.value:", v)
+		fmt.Println(ok)
+	} else {
+		fmt.Println("two' is not in numberMap")
+	}
 }
