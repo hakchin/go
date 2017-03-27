6aecd27dd4239cac9f92567aa550d6b533fc5abb
hakchin <hakchin@gmail.com>
2017-03-27 22:18:48 +0900

    new file:   ../docs/GoBootcamp.epub 	modified:   ../docs/GoBootcamp.mobi 	modified:   ../docs/GoBootcamp.pdf 	new file:   ../docs/build-web-application-with-golang-en.epub 	deleted:    999.go.test/.main.go.swp 	modified:   999.go.test/main.go

--- src/999.go.test/main.go
+++ src/999.go.test/main.go
@@ -3,24 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	numberMap := map[string]int{}
-	numberMap["zero"] = 0
-	numberMap["one"] = 1
-	numberMap["two"] = 2
-	fmt.Println(numberMap["zero"])
-	fmt.Println(numberMap["one"])
-	fmt.Println(numberMap["two"])
+	type rect struct{ w, h float64 }
 
-	if v, ok := numberMap["three"]; ok {
-		fmt.Println("three' is in numberMap.value:", v)
-	} else {
-		fmt.Println("three' is not in numberMap")
-		fmt.Println(ok)
-	}
-	if v, ok := numberMap["two"]; ok {
-		fmt.Println("two' is in numberMap.value:", v)
-		fmt.Println(ok)
-	} else {
-		fmt.Println("two' is not in numberMap")
-	}
+	var pRect *rect
+	var pInt *int
+	var pFloat *float64
+	var pComplex *complex128
 }
