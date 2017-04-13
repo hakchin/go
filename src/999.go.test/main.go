1f625f066907454f4d2b27570534a96faf58740c
hakchin <hakchin@gmail.com>
2017-04-14 06:23:09 +0900

    modified:   main.go

--- src/999.go.test/main.go
+++ src/999.go.test/main.go
@@ -2,14 +2,16 @@ package main
 
 import "fmt"
 
+type rect struct {
+	width  float64
+	height float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
 func main() {
-	type rect struct{ w, h float64 }
-
-	var i int = 1
-	var p *int = &i
-	var s *rect = &rect{1, 2}
-
-	fmt.Println(p)
-	fmt.Println(s)
+	r := rect{3, 4}
+	fmt.Println("area:", r.area())
 
 }
